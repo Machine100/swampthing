2a1a9c4921983dd927c5b63041c3cb21624e28bb
machine100 <joesmileyyy@yahoo.com>
2019-08-20 18:44:52 -0700

    .

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { GhostComponent } from './components/ghost/ghost.component';
+import { VampireComponent } from './vampire/vampire.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GhostComponent,
+    VampireComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/vampire/vampire.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-vampire',
+  templateUrl: './vampire.component.html',
+  styleUrls: ['./vampire.component.css']
+})
+export class VampireComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
